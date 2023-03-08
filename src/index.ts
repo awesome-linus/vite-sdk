24530397189a1945f74dd13269c422962cd86fd5
kou <contact@awesome-linus.com>
2023-03-09 00:19:49 +0900

    install outer library

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const showIframe = (elementId: string)  => {
   const iframe = document.createElement("iframe");
   iframe.src = "https://www.youtube.com/embed/WPK50Qtz2Z0";
@@ -6,4 +8,11 @@ const showIframe = (elementId: string)  => {
   document.getElementById(elementId)?.appendChild(iframe);
 }
 
+const testOuterLib = async () => {
+  const res = await axios.get("https://api.github.com/octocat")
+  console.log(res.data);
+  console.log(res.status);
+}
+
 window.showIframe = showIframe
+window.testOuterLib = testOuterLib
